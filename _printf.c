2221d129aca38f74cb4ce6c318c39a7b1d2bf454
NourOmarSakr <nour.van.sakr@gmail.com>
2023-08-22 19:26:40 -0700

    First trial

---
+++ _printf.c
@@ -0,0 +1,80 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: a character string composed of zero or more directives
+ *
+ * Return: the number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int count = 0;
+	char c;
+	char *s;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					c = va_arg(args, int);
+					_putchar(c);
+					count++;
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					if (s == NULL)
+						s = "(null)";
+					while (*s != '\0')
+					{
+						_putchar(*s);
+						s++;
+						count++;
+					}
+					break;
+				case '%':
+					_putchar('%');
+					count++;
+					break;
+				default:
+					_putchar('%');
+					_putchar(format[i]);
+					count += 2;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
